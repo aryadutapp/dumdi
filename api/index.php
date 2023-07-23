aa9a54bae7be2d18c348a927faba51a6be05e1ed
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-23 23:10:00 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -7,25 +7,37 @@ $dbname = 'vercel_db';
 $user = 'default';
 $password = 'xXk9cTjer8uA';
 
-try {
-    // Establish a connection to the PostgreSQL database with SSL options
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password;sslmode=require");
+// Replace the following variables with your actual PostgreSQL database credentials
+$connectionString = "host=your_postgres_host port=your_postgres_port dbname=your_database_name user=your_database_user password=your_database_password";
 
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    // Establish a connection to the PostgreSQL database using libpq key=value syntax
+    $conn = pg_connect($connectionString);
+
+    if (!$conn) {
+        echo "Connection failed";
+        exit;
+    }
 
     // Prepare and execute the query to fetch all data from the table
-    $stmt = $pdo->query("SELECT * FROM your_table_name");
+    $query = "SELECT * FROM your_table_name";
+    $result = pg_query($conn, $query);
 
-    // Fetch all rows as an associative array
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (!$result) {
+        echo "Query execution failed";
+        exit;
+    }
 
     // Echo the data
-    foreach ($rows as $row) {
+    while ($row = pg_fetch_assoc($result)) {
         echo "ID: {$row['id']}, Full Name: {$row['full_name']}, Gender: {$row['gender']}<br>";
     }
 
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    // Close the connection
+    pg_close($conn);
+
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
 ?>
+
