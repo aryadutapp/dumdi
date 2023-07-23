6334312f2e4422a1a92751475f7b3b07ad24f9cf
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-23 23:31:40 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,38 +1,35 @@
 <?php
-// Replace the following variables with your actual PostgreSQL database credentials
-$endpointId = 'ep-odd-paper-540852'; // Endpoint ID from your PostgreSQL database URL
-$dbname = 'vercel_db';
-$user = 'default';
-$password = 'xXk9cTjer8uA';
+// ElephantSQL database credentials
+$host = 'stampy.db.elephantsql.com';
+$port = '5432';
+$username = 'kljxcbtt';
+$password = 'PZoyG78XuGYVBSJd6olJX4c73C1yduYV';
+$dbname = 'kljxcbtt';
 
-try {
-    // Establish a connection to the PostgreSQL database using libpq key=value syntax
-    $connectionString = "host={$endpointId}.us-east-1.postgres.vercel-storage.com dbname=$dbname user=$user password=$password options=--cluster={$endpointId} sslmode=require";
-    $conn = pg_connect($connectionString);
+// Establish a connection to the database
+$db = pg_connect("host=$host port=$port dbname=$dbname user=$username password=$password");
 
-    if (!$conn) {
-        echo "Connection failed";
-        exit;
-    }
-
-    // Prepare and execute the query to fetch all data from the table
-    $query = "SELECT * FROM your_table_name"; // Replace "your_table_name" with your actual table name
-    $result = pg_query($conn, $query);
-
-    if (!$result) {
-        echo "Query execution failed";
-        exit;
-    }
-
-    // Echo the data
-    while ($row = pg_fetch_assoc($result)) {
-        echo "ID: {$row['id']}, Full Name: {$row['full_name']}, Gender: {$row['gender']}<br>";
-    }
-
-    // Close the connection
-    pg_close($conn);
-
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
+// Check if the connection was successful
+if (!$db) {
+    die("Connection failed: " . pg_last_error());
 }
+
+// Prepare the SQL query
+$query = "SELECT * FROM users;";
+
+// Execute the query
+$result = pg_query($db, $query);
+
+// Check if the query was executed successfully
+if (!$result) {
+    die("Query failed: " . pg_last_error());
+}
+
+// Fetch and display the data
+while ($row = pg_fetch_assoc($result)) {
+    echo "Full Name: " . $row['full_name'] . ", Gender: " . $row['gender'] . "<br>";
+}
+
+// Close the database connection
+pg_close($db);
 ?>
