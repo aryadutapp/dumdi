e98b1f73264a601f046fb8261fcf8ebb9219b9e3
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-24 11:28:38 +0700

    Update tes.php

--- api/tes.php
+++ api/tes.php
@@ -14,13 +14,24 @@ if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
 
-// Prepare the SQL query
-$query = "SELECT * FROM data_customer;";
+// Prepare the SQL query to insert a new record
+$insertQuery = "INSERT INTO data_customer (full_name, gender) VALUES ('jamal', 'M');";
 
-// Execute the query
-$result = pg_query($conn, $query);
+// Execute the insert query
+$insertResult = pg_query($conn, $insertQuery);
 
-// Check if the query was executed successfully
+// Check if the insert query was executed successfully
+if (!$insertResult) {
+    die("Insert query failed: " . pg_last_error());
+}
+
+// Prepare the SQL query to select all records from the table
+$selectQuery = "SELECT * FROM data_customer;";
+
+// Execute the select query
+$result = pg_query($conn, $selectQuery);
+
+// Check if the select query was executed successfully
 if (!$result) {
     die("Query failed: " . pg_last_error());
 }
